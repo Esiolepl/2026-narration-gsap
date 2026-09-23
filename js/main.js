6789ab566c3e222952e937ebcb035f3d57d90dbf
Eloïse Poulage-Lisi <e.poulagelisi@gmail.com>
2026-09-23 14:57:26 +0200

    gsap, js, and html dev, added sections

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
-/* Import ******************************/
+/* Import *************************************/
 
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
 
-/* Variables **************************/
+/* Variables **********************************/
 
 
-/* Fonctions **************************/
+/* Fonctions **********************************/
 
-/* Animation 2e block *****/
+/* Animation 1er block ------------------*/
 
-gsap.to("#moving-block2", { /* Va à ..., contrairement à "from" qui part de ... */
+
+
+/* Animation 2e block (ScrollTrigger) --------------*/
+
+gsap.to("#moving-block2", { // Va à ..., contrairement à "from" qui part de ...
     scrollTrigger : {
         trigger : "#section2",
         start : 'top 50%',
-        markers : true,
-        scrub : 1,
-        toggleActions : 'play none reverse reset',
+        markers : false, //"true" pour montrer les marqueurs de ref d'animation (start & end), "false" pour les cacher
+        scrub : 1, // selon la valeur, joue les animations de meme classe de manière saccadée
+        toggleActions : 'play none reverse reset', // Permet que l'animation suive le scroll
     },
 
     y : '50vh',
     rotation : 360,
     duration : 2,
 })
+
+/* Animation 3e block (Timeline) ----------------*/
+
+let tl = gsap.timeline({
+    repeat : -1,
+    // yoyo : true, permet que l'animation se joue aussi dans le sens inverse infiniement
+})
+
+tl.to("#moving-block3", {x : "100px",})
+.to("#moving-block3", {y : "100px",})
+.to("#moving-block3", {x : "-20px",})
+.to("#moving-block3", {y : "-1px",})
+
+/* Animation 4e block (Draggable) ----------------*/
+
+Draggable.create("#moving-block4", {
+    type : 'y', //objet uniquement draggable sur l'axe vertical
+    bounds : "#section4" //interdit au block de sortir de la zone indiquée
+})
+
 
 
 
