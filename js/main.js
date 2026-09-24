7bba8009021bd8cdbff8fe656c73e76d1f578122
Eloïse Poulage-Lisi <e.poulagelisi@gmail.com>
2026-09-24 13:54:13 +0200

    added section 4 and its gsap animation

--- js/main.js
+++ js/main.js
@@ -104,10 +104,64 @@ tl.timeScale(0.5); // Ralenti le mouvement du block mouvant
 
 /* Animation 4e section (Draggable) ----------------*/
 
-Draggable.create("#moving-block4", {
-    type : 'y', //objet uniquement draggable sur l'axe vertical
-    bounds : "#section4" //interdit au block de sortir de la zone indiquée
-})
+let pointsDeSucces = 0;
+const pairesTotales = 3;
+
+Draggable.create(".draggable-card", {
+  type: "x,y",
+  edgeResistance: 0.65,
+  bounds: "#section4", // Limite le déplacement à la section
+  
+  onDragEnd: function() { // S'occupe des éléments lorsqu'on les lache quelque part
+    const draggedElem = this.target;
+    const targetKey = draggedElem.getAttribute("data-target");
+    const targetZone = document.querySelector(`.target-zone[data-target="${targetKey}"]`); // Sélectionne la zone de dépôt correspondante
+    
+    if (this.hitTest(targetZone, "50%")) { // Vérifie si la  carte touche la bonne zone cible (au moins 50% de chevauchement)
+      
+      const targetBounds = targetZone.getBoundingClientRect(); // Aligne parfaitement l'élément sur la zone cible
+      const dragBounds = draggedElem.getBoundingClientRect()
+      
+      const xOffset = targetBounds.left - dragBounds.left + this.x; // Calcule la différence de position pour replacer la carte correctement dans la zone
+      const yOffset = targetBounds.top - dragBounds.top + this.y;
+
+      gsap.to(draggedElem, {
+        x: xOffset,
+        y: yOffset,
+        duration: 0.3,
+        ease: "power2.out"
+      });
+
+      this.disable(); // Désactive le drag sur ce bloc
+           
+      targetZone.classList.add("matched"); // Changement de couleur si l'association est correcte
+      gsap.to(draggedElem, { backgroundColor: "#22c55e", scale: 0.95 });
+
+      pointsDeSucces++; // Compte la victoire
+      if (pointsDeSucces === pairesTotales) {
+        onGameComplete();
+      }
+
+    } else { // Si la cible est ratée ou mauvaise, retour à la position initiale  
+      gsap.to(draggedElem, {
+        x: 0,
+        y: 0,
+        duration: 0.5,
+        ease: "back.out(1.5)"
+      });
+    }
+  }
+});
+
+function onGameComplete() { // Animation de victoire globale
+  gsap.to(".game-section h2", {
+    scale: 1.2,
+    color: "#22c55e",
+    duration: 0.5,
+    yoyo: true,
+    repeat: 1
+  });
+}
 
 
 
